7241cb5c0b3bad7fbf6059ec174759f2ba933708
Lauren <laurenmtech@gmail.com>
2025-04-03 19:12:29 -0700

    binary search tree traversal code

--- src/main/java/org/example/sandbox/trees/binarysearchtree/BinarySearchTree.java
+++ src/main/java/org/example/sandbox/trees/binarysearchtree/BinarySearchTree.java
@@ -2,6 +2,7 @@ package org.example.sandbox.trees.binarysearchtree;
 
 import org.example.sandbox.trees.ElementNotFoundException;
 import org.example.sandbox.trees.Tree;
+import org.example.sandbox.trees.binarytree.BinaryTree;
 
 import java.util.*;
 
@@ -176,7 +177,11 @@ public class BinarySearchTree<T extends Comparable> implements Tree<T> {
     }
 
     private void preOrder(BinaryTreeNode<T> node, Queue<T> tempList) {
-        // TODO implement pre order iterator
+        if (node != null) {
+            tempList.add(node.getElement());
+            preOrder(node.getLeft(), tempList);
+            preOrder(node.getRight(), tempList);
+        }
     }
 
     public Iterator<T> iteratorInOrder() {
@@ -188,7 +193,11 @@ public class BinarySearchTree<T extends Comparable> implements Tree<T> {
     }
 
     private void inOrder(BinaryTreeNode<T> node, Queue<T> tempList) {
-        // TODO implement in order iterator
+        if (node != null) {
+            inOrder(node.getLeft(), tempList);
+            tempList.add(node.getElement());
+            inOrder(node.getRight(), tempList);
+        }
     }
 
     public Iterator<T> iteratorPostOrder() {
@@ -200,7 +209,11 @@ public class BinarySearchTree<T extends Comparable> implements Tree<T> {
     }
 
     private void postOrder(BinaryTreeNode<T> node, Queue<T> tempList) {
-        // TODO implement post order iterator
+        if (node != null) {
+            postOrder(node.getLeft(), tempList);
+            postOrder(node.getRight(), tempList);
+            tempList.add(node.getElement());
+        }
     }
 
     public Iterator<T> iteratorLevelOrder() {
@@ -213,7 +226,25 @@ public class BinarySearchTree<T extends Comparable> implements Tree<T> {
 
     private void levelOrder(BinaryTreeNode<T> node, Queue<T> tempList) {
 
-        // TODO implement level order iterator
+        Queue<BinaryTreeNode<T>> queue = new LinkedList<>();
+        queue.add(node);
+
+        // while we have nodes to iterate over...
+        while (!queue.isEmpty()) {
+
+            // store node value
+            BinaryTreeNode<T> tempNode = queue.poll(); // pulling current node from queue
+            tempList.add(tempNode.getElement());
+
+            if (tempNode.left != null) {
+                queue.add(tempNode.left); // add left child to queue
+            }
+
+            if (tempNode.right != null) {
+                queue.add(tempNode.right); // add right child to queue
+            }
+
+        }
 
     }
 
